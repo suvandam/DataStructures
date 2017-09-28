aa96f85cbc7d784fe8fd63a654807756470f0b2d
suryaverma <suryaverma@192.168.0.30>
2017-09-27 23:33:51 -0700

    Second

---
+++ DataStructureAlgo/src/com/ds/linkedlist/TestLinkedList.java
@@ -0,0 +1,10 @@
+package com.ds.linkedlist;
+
+public class TestLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
